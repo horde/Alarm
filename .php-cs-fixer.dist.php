14cdd1b5c3b3d693a3681d70b5a2cc11c28320e7
Ralf Lang <ralf.lang@ralf-lang.de>
2025-05-05 16:01:27 +0200

    fix: Add meta files from 2025 pipeline

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,25 +1,21 @@
 <?php
+$potentialDirs = ['/lib', '/src', '/tests'];
 
-declare(strict_types=1);
+$finder = (new PhpCsFixer\Finder());
 
-$finder = PhpCsFixer\Finder::create()
-    // Lib is for legacy
-    ->exclude('lib')
-        // doc may contain ancient examples or illustrational shorthand
-    ->exclude('doc')
-    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
-    ->in(__DIR__)
-;
+foreach ($potentialDirs as $dir) {
+    $full = __DIR__ . $dir;
+    if (is_dir($full)) {
+        $finder->in($full);
+    }
+}
 
-$config = new PhpCsFixer\Config();
-return $config->setRules([
-    '@PSR12' => true,
-    '@PHP83Migration' => true,
-    'ordered_imports' => true,
-    'strict_param' => true,
-    'declare_strict_types' => true,
-    'method_argument_space' => true,
-    'array_syntax' => ['syntax' => 'short'],
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PER-CS' => true,
+        '@PHP82Migration' => true,
+        'php_unit_test_class_requires_covers' => true,
     ])
-    ->setFinder($finder)->setFormat('txt')->setRiskyAllowed(true);
+    ->setFinder($finder)
 ;
